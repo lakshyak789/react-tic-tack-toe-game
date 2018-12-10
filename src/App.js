5710430aa99f1cdc40f383edcc0963e0286c62c3
Lakshya Khanna <rahul.sharma@actionable-science.com>
2018-12-10 18:50:26 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  state = {
+    box: Array(9).fill(""),
+    player: "X",
+    winner: '',
+    winningb0x: [0,1,2]
+  }
+  resetval(){
+    this.setState({
+      box: Array(9).fill(''),
+      winner: []
+  });
+  }
+
+  checkWinner(){
+    let winner = [
+      ["0","1","2"],
+      ["3","4","5"],
+      ["6","7","8"],
+
+      ["0","3","6"],
+      ["1","4","7"],
+      ["2","5","8"],
+
+      ["0","4","8"],
+      ["2","4","6"],
+    ]
+
+    for (let index = 0; index < winner.length; index++) {
+      const [a,b,c] = winner[index];
+      if(this.state.box[a] !=="" && this.state.box[a] === this.state.box[b] && this.state.box[b] === this.state.box[c] && this.state.box[c] === this.state.box[a])
+      {
+        alert("Player " + this.state.player + " won");
+        this.setState({winner:  this.state.player})
+        this.setState(prevstate=>({winningb0x: [...prevstate.winningb0x = a,b,c]}));
+      }
+    }
+  }
+  handleClick(index){
+    if(this.state.box[index] === "" && this.state.winner === '')
+    {
+      let newval = this.state.box;
+      newval[index] = this.state.player;
+      this.setState({
+        player: this.state.player === "X" ? "O" : "X",
+        box: newval
+      })
+      this.checkWinner();
+    }
+  }
+  render(){
+    
+    console.log(this.state.winningb0x);
+    const box = this.state.box.map((box , index) => 
+        <div className ={this.state.winningb0x.includes(index) ? "box win-col" : "box"}  onClick={() => this.handleClick(index)}>{box}</div>)
+        console.log(box);
+    return(
+      <div className="container">
+        <div className="board">
+          {box}
+        </div>
+        <button onClick={() => this.resetval()}>Reset</button>
       </div>
-    );
+    )
   }
 }
-
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,87 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// import React, { Component } from 'react';
+// import './App.css';
+// class App extends Component {
+//   state= {
+//     ,
+//     player: null, 
+//     winner: null,
+//     selectedArr: []
+//   }
+
+//   checkWinner(){
+//     let winlines = [
+//       ["0","1","2"],
+//       ["3","4","5"],
+//       ["6","7","8"],
+
+//       ["0","3","6"],
+//       ["1","4","7"],
+//       ["2","5","8"],
+
+//       ["0","4","8"],
+//       ["2","4","6"],
+//     ]
+//     for (let index = 0; index < winlines.length; index++) {
+//       const [a, b, c] = winlines[index];
+//       if(this.state.box[a] !== null && this.state.box[a] !== null && this.state.box[c] !== null )
+//       {
+//         if(this.state.box[a] === this.state.box[b] && this.state.box[a] === this.state.box[c])
+//         {
+          
+//           this.setState(prevstate => ({selectedArr: [...prevstate.selectedArr = a,b,c]}));
+//           alert("Player" + " " + this.state.player + " won");
+//           this.setState({winner: this.state.player});
+//         }
+//       }
+//     }
+//   }
+//   handleClick(index){
+//     let newvalue = this.state.box;
+//     if(!this.state.player)
+//     {
+//       alert("please select player first");
+     
+//     }
+//     else {
+//       if(this.state.box[index] === null && !this.state.winner)
+//       {
+//         newvalue[index] = this.state.player;
+//         this.setState({
+//           box: newvalue,
+//           player: this.state.player === "X" ? "O" : "X"
+//         });
+//         this.checkWinner()
+//       }
+//     }
+    
+//   }
+//   choosePlayer(e){
+//     this.setState({player: e.target.value});
+//   }
+//   reset(e){
+//     this.setState({box: null });
+//   }
+//   render() {
+//     const box = this.state.box.map((box ,index) => <div className={this.state.selectedArr.includes(index) ? "box selected" : "box"} key={index} onClick={() => this.handleClick(index)}>{box}</div>);
+//     return (
+//       <div className="container"> 
+//         <h1>Tic tac toe game under process </h1>
+//         <form>
+//           <label>Choose Player: </label><br></br>
+//           <label>Choose X</label>  <input type="radio" name="player" value="X" onClick={ this.choosePlayer.bind(this)}/> <br></br>
+//           <label>Choose O</label>  <input type="radio" name="player" value="O" onClick={ this.choosePlayer.bind(this)} /> 
+//         </form>
+//         <div className="board">
+//        {box}
+//         </div>
+//         <button class="reset" onClick={this.reset.bind(this)}>
+//           Reset
+//         </button>
+//       </div>
+//     );
+//   }}
+// export default App;
